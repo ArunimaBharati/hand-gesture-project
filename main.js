ad07525598da79690bac30a890cffa8ef261a04d
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2022-02-18 19:23:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ function take_snapshot()
 
 }
 
-console.log('ml5 version:'.ml5.version);
+console.log('ml5 version:',ml5.version);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tou1HMybP/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oznH91L4X/model.json',modelLoaded);
 
 function modelLoaded()
 {
@@ -35,3 +35,42 @@ function speak()
     synth.speak(utterThis);
 }
 
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="amazing"){
+            document.getElementById("update_emoji").innerHTML="&#128076;";
+        }
+        if(results[0].label=="best"){
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+        }
+        if(results[0].label=="hi"){
+            document.getElementById("update_emoji").innerHTML="&#128075;";
+        }
+        if(results[1].label=="amazing"){
+            document.getElementById("update_emoji2").innerHTML="&#128076;";
+        }
+        if(results[1].label=="best"){
+            document.getElementById("update_emoji2").innerHTML="&#128077;";
+        }
+        if(results[1].label=="hi"){
+            document.getElementById("update_emoji2").innerHTML="&#128075;";
+        }
+    }
+}
